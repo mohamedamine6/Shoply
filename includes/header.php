642fe65264d063201e71c600a86b09cccd2ead69
mohamedamine6 <hachchammohamedamine@gmail.com>
2026-04-23 00:35:53 +0100

    first commit

---
+++ includes/header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/Projet/assets/css/navbar.css">
+    <link rel="stylesheet" href="/Projet/assets/css/variable.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg custom-navbar">
+        <div class="container">
+
+            <a class="navbar-brand fw-bold logo" href="#">Shop<span>ly</span></a>
+
+            <!-- TOGGLE BUTTON (mobile) -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+
+                    <li class="nav-item">
+                        <a href="index.php" class="nav-link max_lg_2 active">Home</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="shop.php" class="nav-link">Shop</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="blog.php" class="nav-link">Blog</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="about.php" class="nav-link">About</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="contact.php" class="nav-link">Contact</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="cart.php" class="nav-link"><i class='bx bx-cart-alt'></i></a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,23 @@
+## Shoply Project
+
+Mini e-commerce website built with:
+- HTML
+- CSS (Custom properties / Variables)
+- javaScript
+- Bootstrap
+- PHP
+- MySQL
+
+## Features
+- Client Side
+- Responsive Navbar
+- Product listing
+- Product details page
+- Shopping cart (session)
+- Admin Panel
+- Dashboard
+- Add / Update / Delete products
+- Search products
+
+## Status
+- In Progress
