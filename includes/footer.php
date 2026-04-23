631e272a5de5b05c34863bc3486f658c8d3c8464
mohamedamine6 <hachchammohamedamine@gmail.com>
2026-04-23 02:31:39 +0100

    finish footer design

---
+++ includes/footer.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/Projet/assets/css/footer.css">
+    <link rel="stylesheet" href="/Projet/assets/css/variable.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <footer class="custom-footer mt-5">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-12 col-md-3">
+                    <!-- Logo -->
+                    <h4 class="footer-logo">Shop<span>ly</span></h4>
+                    <p>Your modern e-commerce store built with passion.</p>
+                </div>
+
+                <div class="col-12 col-md-3">
+                    <h5>Quick Links</h5>
+                    <ul class="footer-links">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="shop.php">Shop</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-12 col-md-3">
+                    <h5>About</h5>
+                    <ul class="footer-links">
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Delivery Information</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Terms & Conditions</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-12 col-md-3">
+                    <h5>Contact</h5>
+                    <ul class="footer-links">
+                        <li><i class='bx bxs-store'></i> Maroc,Rabat</li>
+                        <li><i class='bx bxs-envelope'> Shoply@gmail.com</i></li>
+                        <li><i class='bx bxs-phone'></i> +212 666 677 655</li>
+                        <li><i class='bx bx-import'></i> +212 666 677 655</li>
+                    </ul>
+                </div>
+
+            </div>
+
+                <hr>
+
+                <div class="text-center">
+                    <p class="small text-muted mb-1">© 2026 Shoply. All rights reserved.</p>
+                    <div class="d-flex justify-content-center gap-3">
+                        <a href="#"><i class='bx bxl-facebook-square'></i></a>
+                        <a href="#"><i class='bx bxl-instagram-alt'></i></a>
+                        <a href="#"><i class='bx bxl-twitter'></i></a>
+                    </div>
+                </div>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,12 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="assets/css/variable.css">
+    <link rel="stylesheet" href="Projet/assets/css/variable.css">
+    <link rel="stylesheet" href="Projet/assets/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
+    <!-- navbar -->
     <?php include 'includes/header.php'; ?>
+
+    <!-- footer -->
+    <?php include 'includes/footer.php'; ?>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
